f0780122b90e25d3c3f97e84205caeeb7929ddf1
Sylm@n <l.musilmanbekova12@gmail.com>
2023-08-30 11:49:48 +0300

    Some modified homeWork task with triangle

--- src/AlexeyBulatov/home_work/Triangle.java
+++ src/AlexeyBulatov/home_work/Triangle.java
@@ -1,2 +1,13 @@
-package AlexeyBulatov.home_work;public class Triangle {
+package AlexeyBulatov.home_work;
+
+public class Triangle {
+//    3.	+Написать программу вычисления n-ого треугольного числа.
+    public static void main(String[] args) {
+        int n = 17;
+        int result = 0;
+        for (int i = 1; i < n; i++) {
+            result = (n * (n + 1)) / 2;
+        }
+        System.out.println(result);
+    }
 }
