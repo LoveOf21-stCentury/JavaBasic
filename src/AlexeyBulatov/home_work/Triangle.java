3d44a60931272761cd82710912a623b4970ac07a
Sylm@n <l.musilmanbekova12@gmail.com>
2023-09-01 18:01:23 +0300

    Modified task with Triangle

--- src/AlexeyBulatov/home_work/Triangle.java
+++ src/AlexeyBulatov/home_work/Triangle.java
@@ -1,13 +1,17 @@
 package AlexeyBulatov.home_work;
 
+import java.util.Scanner;
+
 public class Triangle {
-//    3.	+Написать программу вычисления n-ого треугольного числа.
+    //    3.	+Написать программу вычисления n-ого треугольного числа.
     public static void main(String[] args) {
-        int n = 17;
-        int result = 0;
-        for (int i = 1; i < n; i++) {
-            result = (n * (n + 1)) / 2;
-        }
-        System.out.println(result);
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your input");
+        int n = s.nextInt();
+        calculate(n);
+    }
+
+    public static void calculate(int n) {
+        System.out.println(n * (n + 1) / 2);
     }
 }
