425641abb3de7c65bf0f3a56d6bbf230c0cde1f2
Sylm@n <l.musilmanbekova12@gmail.com>
2023-09-06 17:38:56 +0300

    Finished task with arithmetic equation. Les2.home_work

--- src/AlexeyBulatov/Les2/Test.java
+++ src/AlexeyBulatov/Les2/Test.java
@@ -1,18 +1,36 @@
 package AlexeyBulatov.Les2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Test {
+    static char[] textGlobal;
+    static ArrayList<Integer> signIndexesGlobal = new ArrayList<>();
+
     public static void main(String[] args) {
 //        String str = "dab";
 //        char c = str.charAt(0);
 //        String substring = str.substring(1, 2);
 //        System.out.println(substring);
 //        polindrone("mam");
-        int q = 3;
-        int x = 10;
-        int[] array = new int[q];
-        generateCombination(array, 0, x);
+
+        String text = "5? + 1? = ??";
+        ArrayList<Integer> signIndexes = new ArrayList<>();
+
+        char[] textChars = text.replaceAll(" ", "").toCharArray();
+        for (int i = 0; i < textChars.length; i++) {
+            if (textChars[i] == '+' || textChars[i] == '=') {
+                textChars[i] = '-';
+            }
+
+            if (textChars[i] == '?') {
+                signIndexes.add(i);
+            }
+        }
+        textGlobal = textChars;
+        signIndexesGlobal = signIndexes;
+
+        generateCombination(new int[signIndexes.size()], 0, 10);
     }
 
     public static void polindrone(String str) {
@@ -26,19 +44,35 @@ public class Test {
         }
     }
 
-    private static void generateCombination(int array[], int index, int x) {
+    private static void generateCombination(int[] arrays, int index, int k) {
 //        5. *+Задано уравнение вида q + w = e, q, w, e >= 0.
 //        Некоторые цифры могут быть заменены знаком вопроса, например 2? + ?5 = 69.
 //        Требуется восстановить выражение до верного равенства. Предложить хотя бы одно решение или сообщить, что его нет.
-        if (index == array.length) {
-            System.out.println(Arrays.toString(array));
+        if (index == arrays.length) {
+            checkCombination(arrays);
             return;
         }
-        for (int i = 0; i < x; i++) {
-            array[index] = i;
-            generateCombination(array, index + 1, x);
+        for (int i = 0; i < k; i++) {
+            arrays[index] = i;
+            generateCombination(arrays, index + 1, k);
         }
     }
 
+    private static void checkCombination(int[] arrays) {
+        for (int i = 0; i < arrays.length; i++) {
+            textGlobal[signIndexesGlobal.get(i)] = Character.forDigit(arrays[i], 10);
+        }
+
+        String[] numbers = String.valueOf(textGlobal).split("-");
+
+        int a = Integer.parseInt(numbers[0]);
+        int b = Integer.parseInt(numbers[1]);
+        int c = Integer.parseInt(numbers[2]);
+
+        if (a + b == c) {
+            System.out.printf("%d + %d = %d", a, b, c);
+            System.out.println();
+        }
+    }
 
 }
